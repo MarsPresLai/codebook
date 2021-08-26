40c1b83ed094dcf9b01d4d2969237307ab267b41
Mars <38159999+MarsPresLai@users.noreply.github.com>
2021-08-26 11:22:58 +0800

    Update Fenwick_Tree.cpp

--- templates/Data_Structure/Fenwick_Tree.cpp
+++ templates/Data_Structure/Fenwick_Tree.cpp
@@ -5,14 +5,14 @@ struct BIT{
         rt.resize(n + 1);
         sz = n + 1;
     }
-	void add(int i, int v){
-		for(; i < sz; i += i & -i)
-			rt[i] += v;
-	}
-	int sum(int i){
-		int ans  = 0;
-		for(; i; i -= i & -i)
-			ans += rt[i];
-		return ans;
-	}
+    void add(int i, int v){
+        for(; i < sz; i += i & -i)
+	    rt[i] += v;
+    }
+    int sum(int i){
+        int ans  = 0;
+        for(; i; i -= i & -i)
+	    ans += rt[i];
+	return ans;
+    }
 };
