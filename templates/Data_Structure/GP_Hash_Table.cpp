458fde0c568c67e6684c767f3f4bb4e58fe17811
Mars <38159999+MarsPresLai@users.noreply.github.com>
2021-09-04 20:53:23 +0800

    Update GP_Hash_Table.cpp

--- templates/Data_Structure/GP_Hash_Table.cpp
+++ templates/Data_Structure/GP_Hash_Table.cpp
@@ -1,3 +1,5 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 const int RNG = rng();
 struct custom_hash{
