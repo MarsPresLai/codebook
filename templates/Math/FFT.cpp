1fe34ded2c13a2849aa6746453aa7234d91d051a
Mars <38159999+MarsPresLai@users.noreply.github.com>
2022-02-25 18:15:24 +0800

    Update FFT.cpp

--- templates/Math/FFT.cpp
+++ templates/Math/FFT.cpp
@@ -1,38 +1,39 @@
+#define C complex<double>
+ 
 const double pi = acos(-1);
-#define cp complex<double>
-
-void FFT(vector<cp> &a, int inv){
+ 
+void FFT(vector<C> &a, int inv = 1){
     int n = a.size();
-    if(n == 1)return;
-    vector<cp> ch[2];
-    for(int i = 0; i < n; i++){
-        ch[i & 1].push_back(a[i]);
+    for(int i = 1, j = 0; i < n; i++){
+        int bit = n >> 1;
+        for(; bit & j; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+        if(i < j)swap(a[i], a[j]);
     }
-    FFT(ch[0], inv); FFT(ch[1], inv);
-    double ang = 2 * pi / n * inv;
-    cp w(1), rotate(cos(ang), sin(ang));
-    for(int i = 0; i * 2 < n; i++){
-        a[i] = ch[0][i] + w * ch[1][i];
-        a[i + n / 2] = ch[0][i] - w * ch[1][i];
-        if(inv == -1)a[i] /= 2, a[i + n / 2] /= 2;
-        w *= rotate;
+    for(int len = 2; len <= n; len <<= 1){
+        double ang = 2 * pi / len * inv;
+        C rot(cos(ang), sin(ang));
+        for(int i = 0; i < n; i += len){
+            C w(1);
+            for(int j = 0; j < len / 2; j++){
+                C u = a[i + j], v = w * a[i + j + len / 2];
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
+                if(inv == -1)a[i + j] /= 2, a[i + j + len / 2] /= 2;
+                w *= rot;
+            }
+        }
     }
 }
-
-signed main(){
-    setIO();
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n), b(m);
-    for(int &i : a)cin >> i;
-    for(int &i : b)cin >> i;
-    vector<cp> ca(all(a)), cb(all(b));
-    int sz = 1;
-    while(sz < n + m)sz <<= 1;
-    ca.resize(sz), cb.resize(sz);
-    FFT(ca, 1);
-    FFT(cb, 1);
-    for(int i = 0; i < sz; i++)ca[i] *= cb[i];
-    FFT(ca, -1);
-    for(int i = 0; i < n + m - 1; i++)cout << round(ca[i].real()) << ' ';
+ 
+void mul(vector<C> &a, vector<C> &b){
+    int n = a.size() + b.size() - 1;
+    int len = 1 << __lg(n) + 1;
+    a.resize(len);
+    b.resize(len);
+    FFT(a), FFT(b);
+    for(int i = 0; i < len; i++)a[i] *= b[i];
+    FFT(a, -1);
+    a.resize(n);
 }
