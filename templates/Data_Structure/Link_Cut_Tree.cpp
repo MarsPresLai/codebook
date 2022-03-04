fe94511e4e39d06862e5479e0e8c26dbad809ba7
Mars <38159999+MarsPresLai@users.noreply.github.com>
2022-03-04 15:39:08 +0800

    Update Link_Cut_Tree.cpp

--- templates/Data_Structure/Link_Cut_Tree.cpp
+++ templates/Data_Structure/Link_Cut_Tree.cpp
@@ -1,32 +1,70 @@
+// splay
 struct node{
-    int val = 0;
-    node *lc, *rc, *p;
-    node(){lc = rc = p = NULL;}
-    bool lr(){return p && p -> lc = this;}// true l false r
-    void rotate_left(){
-        node *tmp = lc;
-        p -> p = this;
-        lc = p;
-        p -> rc = tmp;
-        if(tmp)tmp -> p = p;
-    }
-    void rotate_right(){
-        node *tmp = rc;
-        p -> p = this;
-        rc = p;
-        p -> lc = tmp;
-        if(tmp)tmp -> p = p;
-    }
-    void rotate(){lr() ? rotate_right() : rotate_left();}
-    void splay(node *top = NULL){
-        node *rt = this;
-        while(rt -> p != top){
-            if(rt -> p -> p != top)
-                (lr() ^ p -> lr() ? p : this) -> rotate();
-            rotate();
+    node *lc, *rc, *pa;
+    int sz, rev;
+    node(){lc = rc = pa = NULL; sz = 1; rev = 0;}
+    bool L(){return pa && pa -> lc == this ? 1 : 0;}
+    bool R(){return pa && pa -> rc == this ? 1 : 0;}
+    bool RT(){return !L() && !R();}
+    void push(){
+        if(rev){
+            if(lc)lc -> rev ^= rev;
+            if(rc)rc -> rev ^= rev;
+            swap(lc, rc);
+            rev = 0;
         }
     }
-    void insert(node *x){
-        
+    node *&ch(bool d){return d ? lc : rc;}
+    void rotate(bool d){
+        node *x = ch(d);
+        if(!RT())pa -> ch(L()) = x;
+        x -> pa = pa;
+        ch(d) = x -> ch(!d);
+        if(x -> ch(!d))x -> ch(!d) -> pa = this;
+        x -> ch(!d) = this;
+        pa = x;
+    }
+    void rotateTop(){pa -> rotate(L());}
+    void pushall(){if(!RT())pa -> pushall(); push();}
+    void splay(){
+        pushall();
+        while(!RT()){
+            if(!pa -> RT())(L() ^ pa -> L() ? this : pa) -> rotateTop();
+            rotateTop();
+        }
+    }
+};
+
+struct LCT{
+    vector<node> rt;
+    LCT(int n){rt.resize(n);}
+    void access(int v){
+        node *now = &rt[v], *lst = NULL;
+        for(node *x = now; x; x = x -> pa){
+            x -> splay();
+            x -> rc = lst;
+            lst = x;
+        }
+        now -> splay();
+    }
+    void makert(int v){
+        access(v);
+        rt[v].rev ^= 1;
+    }
+    void link(int u, int v){
+        makert(u);
+        rt[u].pa = &rt[v];
+    }
+    void cut(int u, int v){
+        makert(u);
+        access(v);
+        if(rt[v].lc){
+            rt[v].lc = rt[v].lc -> pa = NULL;
+        }
+    }
+    bool con(int u, int v){
+        access(u);
+        access(v);
+        return rt[u].pa;
     }
 };
